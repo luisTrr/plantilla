ed16eec9a1f0db3a8cdeddcef2a7d116fc90f5a4
luisTrr <drax026@gmail.com>
2023-08-30 20:20:34 -0400

    consulta agregaras

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cursos;
 
 class CursosController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $datos = $request -> all();
+
+        $curso = new Cursos();
+        $curso->descripcion = $datos['descripcion'];
+        $curso->grado = $datos['grado'];
+        $curso->save();
+
+        return response()->json(['message' => 'Curso creado con éxito']);
+    }
 }
--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -1,10 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Estudiante;
 
 use Illuminate\Http\Request;
 
 class EstudianteController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $datos = $request -> all();
+
+        $estudiante = new Estudiante();
+        $estudiante->nombre = $datos['nombre'];
+        $estudiante->ci = $datos['ci'];
+        $estudiante->save();
+
+        return response()->json(['message' => 'Estudiante creado con éxito']);
+    }
+    public function estudiantesEnQuintoGrado()
+    {
+        $estudiantes = Estudiante::select('estudiantes.nombre', 'cursos.grado')
+            ->join('estudiante_curso', 'estudiantes.id', '=', 'estudiante_curso.estudiante_id')
+            ->join('cursos', 'estudiante_curso.curso_id', '=', 'cursos.id')
+            ->where('cursos.grado', 5)
+            ->get();
+
+        return ['estudiantes' => $estudiantes];
+    }
+    public function todosGrados()
+    {
+        $estudiantes = Estudiante::select('estudiantes.nombre', 'cursos.grado','cursos.descripcion')
+            ->join('estudiante_curso', 'estudiantes.id', '=', 'estudiante_curso.estudiante_id')
+            ->join('cursos', 'estudiante_curso.curso_id', '=', 'cursos.id')
+            ->get();
+
+        return ['estudiantes' => $estudiantes];
+    }
 }
